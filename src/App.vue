21ed0c78a0f9a543fa2a68af9c319525cc98e003
Pavel <hawkrammfan@gmail.com>
2021-07-12 13:11:15 +0300

    component + v-bind + v-on

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <my-component />
+  </div>
+</template>
+
+<script>
+import MyComponent from './components/MyComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyComponent
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="root">
+    <label>
+      Введите email:
+      <input type="number" :value="products" @input="productsInput($event.target.value)" />
+    </label>
+    value: {{ products }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data () {
+    return {
+      products: ''
+    };
+  },
+  methods: {
+    productsInput(value) {
+      if (value >= 0 && value < 11) {
+      this.products = value;
+      }
+      else alert('Не то число!')
+    }
+  },
+};
+</script>
